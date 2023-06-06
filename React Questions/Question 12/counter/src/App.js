3429d01fa3bb16cd90ae112b65c1310e2fb942fb
parasjogani <paras.jogani@gmail.com>
2023-06-06 15:39:37 +0530

    answer 12 added

---
+++ React Questions/Question 12/counter/src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import './App.css';
+import Counter from './component/Counter';
+
+function App() {
+  return (
+    <div className='counter'>
+      <Counter />
+    </div>
+  );
+}
+
+export default App;
+
