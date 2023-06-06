f700c452b931c4b182572cd891ce46c0e799d244
parasjogani <paras.jogani@gmail.com>
2023-06-06 22:39:41 +0530

    answer16 added

---
+++ React Questions/Question 16/task-manager/src/App.js
@@ -0,0 +1,12 @@
+import './App.css';
+import TaskManager from './components/Taskmanager';
+
+function App() {
+  return (
+    <div>
+      <TaskManager />
+    </div>
+  );
+}
+
+export default App;
